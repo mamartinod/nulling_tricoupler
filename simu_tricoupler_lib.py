478b50cf91b432f781ac132d2ee239f321d6fce7
David Sweeney <dswe8949@pendragon.physics.usyd.edu.au>
2021-03-29 11:43:11 +1100

    Removing cupy

--- simu_tricoupler_lib.py
+++ simu_tricoupler_lib.py
@@ -6,7 +6,7 @@ Created on Tue Feb 16 17:26:34 2021
 @author: mam
 """
 import numpy as np
-import cupy as cp
+# import cupy as cp
 from itertools import combinations
 
 def atmo_screen(isz, ll, r0, L0, fc=19.5, correc=1.0, pdiam=None, seed=None):
@@ -33,7 +33,7 @@ def atmo_screen(isz, ll, r0, L0, fc=19.5, correc=1.0, pdiam=None, seed=None):
     If pdiam is not specified, the code assumes that the diameter of
     the pupil is equal to the extent of the phase screen "ll".
     ----------------------------------------------------------- '''
-    
+
     if not seed is None:
         np.random.seed(seed)
 
@@ -62,7 +62,7 @@ def atmo_screen(isz, ll, r0, L0, fc=19.5, correc=1.0, pdiam=None, seed=None):
 def generatePhaseScreen(wavel_r0, isz, ll, r0, L0, fc=19.5, correc=1., pdiam=None, seed=None):
     """
     Generate phase screen in meter.
-    
+
     :param wavel_r0: DESCRIPTION
     :type wavel_r0: TYPE
     :param isz: DESCRIPTION
@@ -88,7 +88,7 @@ def generatePhaseScreen(wavel_r0, isz, ll, r0, L0, fc=19.5, correc=1., pdiam=Non
     phs_screen = atmo_screen(isz, ll, r0, L0, fc=fc, correc=correc, pdiam=pdiam, seed=seed)
     phs_screen = phs_screen.real * wavel_r0 / (2*np.pi)
     return phs_screen
-    
+
 def movePhaseScreen(phase_screens, v_wind, angle_wind, time, meter2pixel):
     '''
     Parameters
@@ -107,15 +107,15 @@ def movePhaseScreen(phase_screens, v_wind, angle_wind, time, meter2pixel):
     Returns
     -------
     moved_phase_screen : array
-        Moved phase screen.   
+        Moved phase screen.
     '''
 
     # phase_screens = cp.array(phase_screens, dtype=cp.float32)
     yshift_in_pix = int(np.around(v_wind * time * meter2pixel * np.sin(np.radians(angle_wind))))
     xshift_in_pix = int(np.around(v_wind * time * meter2pixel * np.cos(np.radians(angle_wind))))
-        
-    return cp.roll(phase_screens, (yshift_in_pix,xshift_in_pix), axis=(-2,-1)), (xshift_in_pix, yshift_in_pix)
-    
+
+    return np.roll(phase_screens, (yshift_in_pix,xshift_in_pix), axis=(-2,-1)), (xshift_in_pix, yshift_in_pix)
+
 def uniform_disk(ysz, xsz, radius, rebin, between_pix=False, norm=False):
     ''' ---------------------------------------------------------
     returns an (ys x xs) array with a uniform disk of radius "radius".
@@ -123,7 +123,7 @@ def uniform_disk(ysz, xsz, radius, rebin, between_pix=False, norm=False):
     xsz2 = xsz * rebin
     ysz2 = ysz * rebin
     radius2 = radius * rebin
-    
+
     if between_pix is False:
         xx,yy  = np.meshgrid(np.arange(xsz2)-xsz2//2, np.arange(ysz2)-ysz2//2)
     else:
@@ -135,7 +135,7 @@ def uniform_disk(ysz, xsz, radius, rebin, between_pix=False, norm=False):
     res = res.mean(3).mean(1)
     if norm:
         res = res / (np.sum(res))**0.5
-    
+
     return(res)
 
 def createSubPupil(sz, sub_rad, baseline, rebin, between_pix=False, norm=False):
@@ -149,15 +149,15 @@ def createSubPupil(sz, sub_rad, baseline, rebin, between_pix=False, norm=False):
     return pupil
 
 def calculateInjection(phs_screen, wl, geo_inj=0.8):
-    rms = cp.std(phs_screen)
-    strehl = cp.exp(-(2*np.pi/wl)**2 * rms**2)
+    rms = np.std(phs_screen)
+    strehl = np.exp(-(2*np.pi/wl)**2 * rms**2)
     injections = geo_inj * strehl
     return injections
-    
+
 def binning(arr, binning, axis=0, avg=False):
     """
     Bin frames together
-    
+
     :Parameters:
         **arr**: nd-array
             Array containing data to bin
@@ -168,16 +168,16 @@ def binning(arr, binning, axis=0, avg=False):
         **avg**: bol
             If ``True``, the method returns the average of the binned frame.
             Otherwise, return its sum.
-            
+
     :Attributes:
         Change the attributes
-        
-        **data**: ndarray 
+
+        **data**: ndarray
             datacube
     """
     if binning is None:
         binning = arr.shape[axis]
-        
+
     shape = arr.shape
     crop = shape[axis]//binning*binning # Number of frames which can be binned respect to the input value
     arr = np.take(arr, np.arange(crop), axis=axis)
@@ -190,29 +190,29 @@ def binning(arr, binning, axis=0, avg=False):
         arr = arr.sum(axis=axis+1)
     else:
         arr = arr.mean(axis=axis+1)
-    
+
     return arr
 
 def addPhotonNoise(data, QE, enf=None):
     if enf is None:
-        noisy_data = cp.random.poisson(data*QE, size=data.shape, dtype=cp.float32)
+        noisy_data = np.random.poisson(data*QE, size=data.shape, dtype=np.float32)
     else:
-        noisy_data = cp.random.poisson(data*QE*enf, size=data.shape, dtype=cp.float32)
-    
+        noisy_data = np.random.poisson(data*QE*enf, size=data.shape, dtype=np.float32)
+
     return noisy_data
 
 def _addRON(data, ron, offset):
-    ron_noise = cp.random.normal(offset, ron, data.shape)
+    ron_noise = np.random.normal(offset, ron, data.shape)
     noisy_data = data + ron_noise
     return noisy_data
 
 def _addDark(ndark, fps, shape, enf=None):
     dark_electrons = ndark / fps
     if enf is None:
-        noisy_data = cp.random.poisson(dark_electrons, size=shape, dtype=cp.float32)
+        noisy_data = np.random.poisson(dark_electrons, size=shape, dtype=np.float32)
     else:
-        noisy_data = cp.random.poisson(dark_electrons*enf, size=shape, dtype=cp.float32)
-    
+        noisy_data = np.random.poisson(dark_electrons*enf, size=shape, dtype=np.float32)
+
     return noisy_data
 
 def addDetectorNoise(data, ron, ndark, fps, enf=None):
@@ -228,90 +228,90 @@ def addNoise(data, QE, read_noise, ndark, fps, activate_photon_noise, activate_d
             noisy = addPhotonNoise(data, 1, enf)
     else:
         noisy = data
-        
+
     if activate_detector_noise:
         if activate_photon_noise:
             noisy = addDetectorNoise(noisy, read_noise, ndark, fps, enf)
         else:
             noisy = addDetectorNoise(noisy*QE, read_noise, ndark, fps, enf)
 
-    return noisy    
-    
+    return noisy
+
 def measurePhase(data, wl):
     di = data[0] - data[2]
     denom = (data[0]+data[2]-2*data[1])
     di2 = di / denom
-    dphi = cp.arctan(-3**0.5*di2)
-    dphi = dphi / (2*np.pi) * wl    
-    return cp.array([dphi], dtype=cp.float32)
+    dphi = np.arctan(-3**0.5*di2)
+    dphi = dphi / (2*np.pi) * wl
+    return np.array([dphi], dtype=np.float32)
 
 def measurePhase2(data, wl):
     di = data[0] - data[2]
     denom = (data[0]+data[2]-2*data[1])
     di2 = di / denom
-    dphi = cp.arctan(-3**0.5*di2)
+    dphi = np.arctan(-3**0.5*di2)
 
-    pij = cp.array([(dphi[i+1] - dphi[i]) / (2*np.pi*(1/wl[i+1] - 1/wl[i])) for i in range(wl.size-1)], dtype=cp.float32)
-    ddphi = cp.array([abs(dphi[i] - dphi[i+1]) for i in range(wl.size-1)], dtype=cp.float32)
-    
-    weight = cp.zeros_like(ddphi)
+    pij = np.array([(dphi[i+1] - dphi[i]) / (2*np.pi*(1/wl[i+1] - 1/wl[i])) for i in range(wl.size-1)], dtype=np.float32)
+    ddphi = np.array([abs(dphi[i] - dphi[i+1]) for i in range(wl.size-1)], dtype=np.float32)
+
+    weight = np.zeros_like(ddphi)
     weight[ddphi<2*np.pi/3] = 1.
-    
-    ddm = cp.sum(weight * pij, 0) / cp.sum(weight, 0)
-    
-    return cp.array([ddm], dtype=cp.float32)
+
+    ddm = np.sum(weight * pij, 0) / np.sum(weight, 0)
+
+    return np.array([ddm], dtype=np.float32)
 
 def measurePhase3(data, wl):
     di = data[0] - data[2]
     denom = (data[0]+data[2]-2*data[1])
     di2 = di / denom
-    dphi = cp.arctan(-3**0.5*di2)
-    
-    pij = cp.diff(dphi) / cp.array([(2*np.pi*(1/wl[i+1] - 1/wl[i])) for i in range(wl.size-1)], dtype=cp.float32)
-    ddphi = cp.abs(cp.diff(dphi))
-    
-    weight = cp.zeros_like(ddphi)
+    dphi = np.arctan(-3**0.5*di2)
+
+    pij = np.diff(dphi) / np.array([(2*np.pi*(1/wl[i+1] - 1/wl[i])) for i in range(wl.size-1)], dtype=np.float32)
+    ddphi = np.abs(np.diff(dphi))
+
+    weight = np.zeros_like(ddphi)
     weight[ddphi<2*np.pi/3] = 1.
-    
-    ddm = cp.sum(weight * pij, 0) / cp.sum(weight, 0)
-    
-    return cp.array([ddm], dtype=cp.float32)
+
+    ddm = np.sum(weight * pij, 0) / np.sum(weight, 0)
+
+    return np.array([ddm], dtype=np.float32)
 
 def selectCoupler(name):
     if name == 'tricoupler':
-        return 1/3**0.5 * cp.array([[1.0                  , cp.exp(1j* 2*cp.pi/3)],
-                                    [cp.exp(1j* 2*cp.pi/3), cp.exp(1j* 2*cp.pi/3)],
-                                    [cp.exp(1j* 2*cp.pi/3), 1.]], dtype=cp.complex64)
+        return 1/3**0.5 * np.array([[1.0                  , np.exp(1j* 2*np.pi/3)],
+                                    [np.exp(1j* 2*np.pi/3), np.exp(1j* 2*np.pi/3)],
+                                    [np.exp(1j* 2*np.pi/3), 1.]], dtype=np.complex64)
     else:
-        return 1/2**0.5 * cp.array([[1.0                  , cp.exp(-1j* cp.pi/2)],
-                                    [cp.exp(-1j* cp.pi/2)  , 1.],
-                                    [0.                   , 0.]], dtype=cp.complex64)
- 
-    
+        return 1/2**0.5 * np.array([[1.0                  , np.exp(-1j* np.pi/2)],
+                                    [np.exp(-1j* np.pi/2)  , 1.],
+                                    [0.                   , 0.]], dtype=np.complex64)
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    
+
     wl0 = 1.6e-6
     bandwidth = 200e-9
     dwl = 5e-9
-    wl = cp.arange(wl0-bandwidth/2, wl0+bandwidth/2, dwl, dtype=cp.float32)
-    pistons = cp.linspace(-10*wl0, 10*wl0, 1000, False)
+    wl = np.arange(wl0-bandwidth/2, wl0+bandwidth/2, dwl, dtype=np.float32)
+    pistons = np.linspace(-10*wl0, 10*wl0, 1000, False)
     print('lc', wl0**2/bandwidth, wl0**2/dwl)
-    
+
     coupler = selectCoupler('tricoupler')
-    
-    a_in = cp.array([cp.exp(1j*2*cp.pi/wl[None,:]*pistons[:,None]), cp.ones((pistons.size, wl.size))], dtype=cp.complex64)
-    a_in = cp.transpose(a_in, (2, 0, 1))
+
+    a_in = np.array([np.exp(1j*2*np.pi/wl[None,:]*pistons[:,None]), np.ones((pistons.size, wl.size))], dtype=np.complex64)
+    a_in = np.transpose(a_in, (2, 0, 1))
     a_out = coupler@a_in
-    a_out = cp.transpose(a_out, (1, 2, 0))
-    i_out = cp.abs(a_out)**2
-    
-    ddm = cp.array([measurePhase2(i_out[:,i], wl) for i in range(pistons.size)])
-    ddm2 = cp.array([measurePhase3(i_out[:,i], wl) for i in range(pistons.size)])
-    
+    a_out = np.transpose(a_out, (1, 2, 0))
+    i_out = np.abs(a_out)**2
+
+    ddm = np.array([measurePhase2(i_out[:,i], wl) for i in range(pistons.size)])
+    ddm2 = np.array([measurePhase3(i_out[:,i], wl) for i in range(pistons.size)])
+
     plt.figure()
-    plt.plot(cp.asnumpy(pistons)/wl0, cp.asnumpy(ddm)/wl0)
-    plt.plot(cp.asnumpy(pistons)/wl0, cp.asnumpy(ddm2)/wl0)
+    plt.plot(pistons/wl0, ddm/wl0)
+    plt.plot(pistons/wl0, ddm2/wl0)
     plt.grid()
 
     # plt.figure()
@@ -327,26 +327,25 @@ if __name__ == '__main__':
     # denom = cp.sqrt(3/4 * ((i_out[0]-i_out[2])**2 + 1/3*(i_out[0] + i_out[2] - 2*i_out[1])**2))
     # model_fringes = cp.asnumpy(num / denom)
     # model_fringes = model_fringes.T
-    
+
     # plt.figure()
     # plt.plot(cp.asnumpy(pistons)/wl0, model_fringes.T, alpha=0.5)
     # plt.grid()
-    
+
     # piston_atm = wl/4
     # a_test = cp.array([cp.exp(1j*2*cp.pi/wl*piston_atm), cp.ones((wl.size,))], dtype=cp.complex64)
     # b_test = tricoupler@a_test
     # i_test = cp.abs(b_test)**2
-    
+
     # num2 = i_test[0] + i_test[2] - 2*i_test[1]
     # num2 = i_test[0] - i_test[2]
     # denom2 = cp.sqrt(3/4 * ((i_test[0]-i_test[2])**2 + 1/3*(i_test[0] + i_test[2] - 2*i_test[1])**2))
     # fringe = cp.asnumpy(num2 / denom2)
-    
+
     # fringe = cp.asnumpy((i_test[0] + i_test[2] - 2 * i_test[1]) / (i_test.sum(0)))
-    
+
     # correlation = model_fringes@fringe
-    
+
     # plt.figure()
     # plt.plot(cp.asnumpy(pistons)/wl0, correlation)
     # plt.grid()
-    
